28d06879d6eaf7a8dda5a44fc9df8ab64ff9d1ce
James Isaac <jamesisaac@Jamess-MacBook-Pro.local>
2025-11-12 15:14:33 -0800

    safe

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Polyfills
+      // Polyfill Node globals for browser
       process: "process/browser",
       buffer: "buffer",
     },
