5e4fe3dc14d7ceb19b7df50a0d029993bf681b5b
James Isaac <jamesisaac@Jamess-MacBook-Pro.local>
2025-11-12 15:05:06 -0800

    safe

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-import { Buffer } from "buffer";
-import * as process from "process";
-window.Buffer = Buffer;
-window.process = process;
-
-
 // TON Connect UI Provider
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
